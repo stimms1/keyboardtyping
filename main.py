6525ab99a2e6650434e60cfc4f93676ef99f58f5
stimms1 <bluearinsol0718@gmail.com>
2025-10-15 00:21:00 +0800

    added the compare system now.
    whats missing is the time function and the logic for calculation of the wpm.
    
    also need to display what the user types so that it can reduce mistakes.

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
 from pynput.keyboard import Listener, Key 
+import time 
 userText = "" 
-def on_press(key):
-    global userText 
+quote = "The quick brown fox jumps over the lazy dog."
+i = 0
+        
+def compare(key):
+    global i
+    global quote 
     try: 
         char = key.char
-        userText += char
-    except AttributeError:
-        if key == Key.space:
-            userText += " " 
-        elif key == Key.enter:
-            userText += "\n"
-        elif key == Key.esc:
-            print("Text is done!") # this is temporary lols
-            print(userText)
+
+        if i >= len(quote) - 1 :
+            print("Quote completed!") # add the time here once time is done.
             return False
+        
+        if char == quote[i]:
+            i+=1 
+        else:
+            print(f"Incorrect Character!")
+
+    except AttributeError: 
+        if key == Key.space:
+            if i < len(quote) and quote[i] == " ":
+                i += 1
+            else: 
+                print(f"Incorrect Character!")
         else: 
             pass
-        
 
 
 
-print("Listening now: ")
-listener = Listener(on_press = on_press)
+
+print(f"Try this quote out! {quote}")
+listener = Listener(on_press = compare)
 
 listener.start()
 
