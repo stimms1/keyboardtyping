b123b81bb32fd5d8a7ca27492853b8b4bf099230
stimms1 <bluearinsol0718@gmail.com>
2025-10-15 21:27:30 +0800

    Handled the mistakes better. Added the visual of the typing so that its easier to see what you are typing.

--- main.py
+++ main.py
@@ -7,22 +7,35 @@ i = 0
 def compare(key):
     global i
     global quote 
+    global userText
+
     try: 
         char = key.char
 
         if i >= len(quote) - 1 :
+            userText += char
+            print(f"\nYour input: {userText}")
             print("Quote completed!") # add the time here once time is done.
             return False
         
         if char == quote[i]:
+            userText += char
             i+=1 
+
+            # this is to print the current sentence typed
+            print(f"\rCurrent: {userText}_", end="", flush=True)
         else:
-            print(f"Incorrect Character!")
+            print(f"\n*** INCORRECT CHARACTER! Expected: '{quote[i]}', Got: '{char}' ***")
+            print(f"Current: {userText} <--- ERROR HERE")
+            print(f"\rCurrent: {userText}_", end="", flush=True)
 
     except AttributeError: 
         if key == Key.space:
             if i < len(quote) and quote[i] == " ":
+                userText += " "
                 i += 1
+
+                print(f"\rCurrent: {userText}_", end="", flush=True)
             else: 
                 print(f"Incorrect Character!")
         else: 
@@ -31,9 +44,10 @@ def compare(key):
 
 
 
-print(f"Try this quote out! {quote}")
+print(f"Try this quote out!")
+print(f"Target: {quote}")
+
 listener = Listener(on_press = compare)
 
 listener.start()
-
 listener.join()
